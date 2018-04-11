eaac7494d35f1c961299f484d1414b4ff108d61a
root <astefanescu95@gmail.com>
2018-04-11 23:58:50 +0300

    Removed junk

---
+++ collected_static/js/main.js
@@ -0,0 +1,15 @@
+var aggregator = angular.module('aggregator', ['djng.forms', 'ngCookies'], function($locationProvider){
+  $locationProvider.html5Mode({enabled: true,
+                              requireBase:false});
+});
+
+aggregator.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+    var $cookies;
+    angular.injector(['ngCookies']).invoke(['$cookies', function(_$cookies_) {
+    $cookies = _$cookies_;
+  }]);
+}]);
+
+angular.element(document).ready(function () { $('select').selectpicker("destroy"); $('select').selectpicker("render"); });
