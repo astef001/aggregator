4876ed923445a9104cc6150231f22cd22f308d59
root <astefanescu95@gmail.com>
2018-04-11 21:04:28 +0300

    Added setup.py

---
+++ aggregator/setup.py
@@ -0,0 +1,10 @@
+import os
+from setuptools import find_packages, setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='aggregator',
+    version='0.1',
+)
